0d4e91a8cd9b6724f8e6347c82cd1bbd3b024f9e
[Cursors] <65373746+cursorsdottsx@users.noreply.github.com>
2021-10-26 21:55:29 -0500

    Update simplevm.ts

--- simplevm.ts
+++ simplevm.ts
@@ -1,3 +1,7 @@
+/**
+ * Executes simple instructions, is Turing complete.
+ */
+
 type Increment<X extends number> = [
     1  , 2  , 3  , 4  , 5  , 6  , 7  , 8  , 9  , 10 ,
     11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19 , 20 ,
